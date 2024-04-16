b999414b61315f92eaf6c64c990bd6188cd81cb9
Joseph Ellis <josephellis95@gmail.com>
2024-04-16 11:18:30 -0500

    actions get

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,28 @@
 // Write your "actions" router here!
 const express = require('express')
 
+
 const router = express.Router()
+
+const action = require("./actions-model");
+
+
+router.get("/", (req, res) => {
+    action.get()
+      .then((found) => {
+        res.json(found);
+        console.log(found)
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "The posts information could not be retrieved",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  });
+  
+
+  
 
 module.exports = router
