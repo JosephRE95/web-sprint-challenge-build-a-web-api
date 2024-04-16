10ecebbe1b35ee12180493a70ff733732c066def
Joseph Ellis <josephellis95@gmail.com>
2024-04-16 11:16:01 -0500

    get for prodjects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,4 +3,29 @@ const express = require('express')
 
 const router = express.Router()
 
+const projects = require("./projects-model");
+
+
+
+
+
+
+  router.get("/", (req, res) => {
+    projects.get()
+      .then((found) => {
+        res.json(found);
+        console.log(found)
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "The posts information could not be retrieved",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  });
+
+
+
+
 module.exports = router
