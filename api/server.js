66e398cb59dcedcb556414c7f7aa460fee55b656
Joseph Ellis <josephellis95@gmail.com>
2024-04-16 08:24:49 -0500

    prodjects router

--- api/server.js
+++ api/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const projectsRouter = require('./projects/projects-router')
+
 const server = express();
 
 // Configure your server here
@@ -7,6 +10,8 @@ server.use(express.json());
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file !
+
+server.use('/api/projects', projectsRouter)
 
 server.use('*', (req,res) => {
     res.status(404).json({
